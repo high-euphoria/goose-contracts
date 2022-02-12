64e084e79e183adfa121c6b62dcb97efafbe4dc1
mindfulme <alice.gogolauri@gmail.com>
2022-02-12 18:28:49 +0200

    readme links

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # High Token contracts and yield farm
+
+
+MasterChef: https://snowtrace.io/address/0x2445A4dD10d04815FE82c8D18073931c32B333d5#code
+
+
+
+HighToken: https://snowtrace.io/address/0xe0Cdce574699753827e50cF3ca2E93E0B9306e3a#code
--- truffle-config.js
+++ truffle-config.js
@@ -16,7 +16,7 @@ module.exports = {
   },
   networks: {
     mainnet: {
-      provider: () => new HDWalletProvider("65841762a510a925032ce4653771035dd78d564257e12eaf77c2bb2024262806", `https://api.avax.network/ext/bc/C/rpc`),
+      provider: () => new HDWalletProvider("", `https://api.avax.network/ext/bc/C/rpc`),
       network_id: 43114,
       confirmations: 2,      // # of confs to wait between deployments. (default: 0)
       timeoutBlocks: 300,  // # of blocks before a deployment times out  (minimum/default: 50)
@@ -42,7 +42,7 @@ module.exports = {
       settings: {
        optimizer: {
          enabled: true,
-         runs: 999999999
+         runs: 999
        },
       }
     },
